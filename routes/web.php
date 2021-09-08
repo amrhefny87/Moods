9ca8100ae6919372c72256c35e98bc654abfd1da
unknown <andreabcm16@gmail.com>
2021-09-08 09:21:05 +0200

    actualizar

--- routes/web.php
+++ routes/web.php
@@ -31,10 +31,4 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 
