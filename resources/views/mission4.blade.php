f6d21248ca5beeccb3593ec092b7647a2f26b4b1
unknown <andreabcm16@gmail.com>
2021-09-23 13:25:11 +0200

    corrigiendo errores

--- resources/views/mission4.blade.php
+++ resources/views/mission4.blade.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="{{ secure_secure_asset('css/mission.css') }}" rel="stylesheet" />
+    <link href="{{ secure_asset('css/mission.css') }}" rel="stylesheet" />
     <title>Mission 4</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
         integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ=="
