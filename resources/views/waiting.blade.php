02c96b804ce26660186d34e4c9224d137bdc7056
Andres Patiño <andresp199519@gmail.com>
2021-09-27 11:22:43 +0200

    Se arregló el problema de los dos logouts

--- resources/views/waiting.blade.php
+++ resources/views/waiting.blade.php
@@ -1,41 +1,7 @@
 @extends('layouts.app')
-<<<<<<< HEAD
-
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <link href="{{asset('css/waiting.css')}}" rel="stylesheet"/>
-        <title>Laravel8</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
- *{
-     box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-    }
-        </style>
-
-        <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
-        </style>
-    </head>
-    <body>
-    <div class="preloader mt-5">
-=======
 @section('waiting')
 <div>
     <div class="preloader">
->>>>>>> 225a8ce6e85d845dc4d820b05955e5509283bd28
         <div class="preloader__ring">
             <div class="preloader__sector">L</div>
             <div class="preloader__sector">o</div>
