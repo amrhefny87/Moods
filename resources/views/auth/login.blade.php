5a10ed4b512b39a0a378f4fc91c563406d109e6f
unknown <andreabcm16@gmail.com>
2021-09-27 11:38:33 +0200

    añadiendo secure_

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,9 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
 
-<link href="{{asset('css/login.css')}}" rel="stylesheet"/>
+<link href="{{secure_asset('css/login.css')}}" rel="stylesheet"/>
         <title>Login</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
@@ -101,7 +101,7 @@
         </div>
     </div>
 </div>
-<script src="{{asset('js/app.js')}}">
+<script src="{{secure_asset('js/app.js')}}">
 </script>
 </body>
 </html>
